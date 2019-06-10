15c99cf16984732df5aa8063e7de43ca87949c10
Snehashis <s.roy@leverge.it>
2019-06-09 20:57:33 -0400

    .

--- app/Http/Controllers/ListViewController.php
+++ app/Http/Controllers/ListViewController.php
@@ -4,22 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\House;
-use \Torann\GeoIP\Facades\GeoIP;
 
 class ListViewController extends Controller
 {
     public function listView(){
-
-
-        $location =geoip($_SERVER['REMOTE_ADDR']);
-
-    	$houses = House::where('city', 'like', $location->city)
-                       ->orderBy('created_at', 'desc')->paginate(10);
-       
-        if(!count($houses)){
-            return redirect('/map-view');
-        }
-
+    	
+    	$houses = House::orderBy('created_at', 'desc')->paginate(10);
 
     	return view('list-view.list-view', [
 
