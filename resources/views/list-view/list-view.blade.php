bfb68d683f8f8720112f3589c8046fa5f580d7f0
Snehashis <s.roy@leverge.it>
2019-08-14 10:15:45 -0400

    .

--- resources/views/list-view/list-view.blade.php
+++ resources/views/list-view/list-view.blade.php
@@ -65,7 +65,17 @@
                 </div>
                     @if ($loop->first)
                     <div class="mb-5">
-                        <!---Add here-->
+
+                        <div id="427268205">
+                            <script type="text/javascript">
+                                try {
+                                    window._mNHandle.queue.push(function (){
+                                        window._mNDetails.loadTag("427268205", "300x250", "427268205");
+                                    });
+                                }
+                                catch (error) {}
+                            </script>
+                        </div>
                     </div>
                     @endif
                 @endforeach
