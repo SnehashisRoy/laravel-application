27b2a0aed6f3f10393c0bc5a94e39b13324b0035
Snehashis <s.roy@leverge.it>
2019-09-01 21:52:14 -0400

    .

--- app/Console/Commands/CreateSitemap.php
+++ app/Console/Commands/CreateSitemap.php
@@ -4,6 +4,12 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\SitemapIndex;
+use Carbon\Carbon;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+use App\Models\House;
+ use Psr\Http\Message\UriInterface;
 
 
 
@@ -40,6 +46,53 @@ class CreateSitemap extends Command
      */
     public function handle()
     {
-        SitemapGenerator::create('http://www.banglatoronto.ca')->writeToFile('/var/www/rentBasement/public/sitemap.xml');
+        
+        
+
+        SitemapIndex::create()
+            ->add('/sitemap.xml')
+            ->add('/sitemap_1.xml')
+            ->add('/sitemap_2.xml')
+            ->writeToFile(public_path('sitemapindex.xml'));
+
+        //SitemapGenerator::create('http://www.banglatoronto.ca')->writeToFile('/var/www/rentBasement/public/sitemap.xml');
+
+       
+
+        // SitemapGenerator::create('http://www.banglatoronto.ca')
+        //    ->shouldCrawl(function (UriInterface $url) {
+        //        // All pages will be crawled, except the contact page.
+        //        // Links present on the contact page won't be added to the
+        //        // sitemap unless they are present on a crawlable page.
+               
+        //        return strpos($url->getPath(), '/listing/') === false;
+        //    })
+        //    ->writeToFile('/var/www/rentBasement/public/sitemap.xml');
+
+        $sitemap = Sitemap::create();
+                    
+        
+        House::where('id','<', 50000)->get()->map(function($listing) use($sitemap){
+
+            $sitemap->add(Url::create('/listing/'. $listing->id)) ;
+
+        });
+
+        $sitemap->writeToFile(public_path('sitemap_1.xml'));
+
+        $sitemap = Sitemap::create();
+                    
+        
+        House::where('id','>=', 50000)->get()->map(function($listing) use($sitemap){
+
+            $sitemap->add(Url::create('/listing/'. $listing->id)) ;
+
+        });
+
+        $sitemap->writeToFile(public_path('sitemap_2.xml'));
+
+           
+
+
     }
 }
