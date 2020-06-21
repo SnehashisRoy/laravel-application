0224f8a4fefc53bec16204b1202a59d56da61f87
Snehashis <s.roy@leverge.it>
2020-06-21 13:31:53 -0400

    .

--- resources/views/quiz/quiz.blade.php
+++ resources/views/quiz/quiz.blade.php
@@ -2,11 +2,11 @@
 
 
 @section('vendorcss') 
-    <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+    <meta property="og:url"                content="{{$quiz->url}}" />
     <meta property="og:type"               content="article" />
-    <meta property="og:title"              content="When Great Minds Don’t Think Alike" />
-    <meta property="og:description"        content="How much does culture influence creative thinking?" />
-    <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+    <meta property="og:title"              content="{{$quiz->title}}" />
+    <meta property="og:description"        content="{{$quiz->description}}" />
+    <meta property="og:image"              content="{{$quiz->image_url}}" />
 
     <link rel="stylesheet" type="text/css" href="/js/vendors/angular-social/angular-social.css">
 
