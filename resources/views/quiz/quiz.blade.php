2df1a383831790e1eb4ce5db34f4702834cea0ee
Snehashis <s.roy@leverge.it>
2020-06-21 17:27:29 -0400

    .

--- resources/views/quiz/quiz.blade.php
+++ resources/views/quiz/quiz.blade.php
@@ -34,87 +34,101 @@
 
     
    
-    <div ng-app="myApp" ng-controller="myCtrl" class="container" >
+    <div ng-app="myApp" ng-controller="myCtrl">
         <div class="text-center m-5">
 
             
 
             <h2>${ quiz.title }$ </h2>
         </div>
-                <div ng-repeat="q in quiz.questions"  ng-show="show($index)" style="min-height: 400px;">
-                     <div  class="row" ng-hide="answer[q.id]">
-                         <div class="col-12 col-md-4">
-                             <img src="${ 'http://banglatoronto.ca/'+ quiz.image }$" class="img-fluid">
-                         </div>
-                         <div class="col-12 col-md-8" >
-                             <p>${ q.question}$</p>
-                             
-                             <div class="form-check" ng-repeat = "choice in q.choices">
-                               <label class="form-check-label">
-                                 <input  ng-click= "checkAnswer(choice.id, q.id)" class="from-check-input" type="radio" name="${q.id+'options'}$"> ${ choice.choice}$
-                               </label>
-                             </div>
-                         </div>
-                    </div>
-                    <div class="row" ng-show="answer[q.id]">
-                        <div class="col-12 col-md-3">
-                            <p> 
-                             <span ng-if="answer[q.id] == 'wrong' " style="color: red;"><i class="fas fa-times-circle fa-2x"></i></span>
-                             <span ng-if="answer[q.id] == 'correct' "style="color: green;"><i class="fas fa-check-circle fa-2x"></i></span> Answer: ${answer[q.id]}$
-                             </p>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-9">
+                        <div ng-repeat="q in quiz.questions"  ng-show="show($index)" style="min-height: 400px;">
+                             <div  class="row" ng-hide="answer[q.id]">
+                                 <div class="col-12 col-md-4">
+                                     <img src="${ 'http://banglatoronto.ca/'+ quiz.image }$" class="img-fluid">
+                                 </div>
+                                 <div class="col-12 col-md-8" >
+                                     <p>${ q.question}$</p>
+                                     
+                                     <div class="form-check" ng-repeat = "choice in q.choices">
+                                       <label class="form-check-label">
+                                         <input  ng-click= "checkAnswer(choice.id, q.id)" class="from-check-input" type="radio" name="${q.id+'options'}$"> ${ choice.choice}$
+                                       </label>
+                                     </div>
+                                 </div>
+                            </div>
+                            <div class="row" ng-show="answer[q.id]">
+                                <div class="col-12 col-md-3">
+                                    <p> 
+                                     <span ng-if="answer[q.id] == 'wrong' " style="color: red;"><i class="fas fa-times-circle fa-2x"></i></span>
+                                     <span ng-if="answer[q.id] == 'correct' "style="color: green;"><i class="fas fa-check-circle fa-2x"></i></span> Answer: ${answer[q.id]}$
+                                     </p>
+                                </div>
+                                 <div class="col-12 col-md-9" >
+                                     <p>${ q.question}$</p>
+                                     <p>${q.explanation}$</p>
+                                     <button ng-click="next()" type="button" class="btn btn-success">Next</button>
+                                 </div>
+                            </div>
+
+
+                            
                         </div>
-                         <div class="col-12 col-md-9" >
-                             <p>${ q.question}$</p>
-                             <p>${q.explanation}$</p>
-                             <button ng-click="next()" type="button" class="btn btn-success">Next</button>
-                         </div>
+
+                        <div  class="container text-center" ng-if = "resultSlide">
+
+                            <div class="row justify-content-center">
+
+                                <div class="col"> <h2>আপনার স্কোর</h2> </div>
+                                
+                            </div>
+
+                            <div class="row justify-content-center">
+
+                                <div class="col">
+                                    <div class="semi-donut-model-2 margin"
+                                         style="--percentage : ${ resultPercent }$ ; --fill: #039BE5 ;">
+                                       ${ resultPercent + '%'}$
+                                    </div>
+                                </div>
+                                
+                            </div>
+
+                            <div class="row justify-content-center">
+
+                                <div class="col mt-5 mb-5">
+                                    <p>বন্ধুদের সাথে শেয়ার করুন</p>
+                                    <ul ng-social-buttons>
+                                         
+                                        <li class="ng-social-facebook">Facebook</li>
+                                        <li class="ng-social-twitter">Twitter</li>
+                                        <li class="ng-social-pinterest">Pinterest</li>
+                                    </ul> 
+
+
+                                </div>
+                                
+                            </div>
+
+                            
+
+                            
+
+                                
                     </div>
-
-
                     
                 </div>
-
-                <div  class="container text-center" ng-if = "resultSlide">
-
-                    <div class="row justify-content-center">
-
-                        <div class="col"> <h2>আপনার স্কোর</h2> </div>
-                        
-                    </div>
-
-                    <div class="row justify-content-center">
-
-                        <div class="col">
-                            <div class="semi-donut-model-2 margin"
-                                 style="--percentage : ${ resultPercent }$ ; --fill: #039BE5 ;">
-                               ${ resultPercent + '%'}$
-                            </div>
-                        </div>
-                        
-                    </div>
-
-                    <div class="row justify-content-center">
-
-                        <div class="col mt-5 mb-5">
-                            <p>বন্ধুদের সাথে শেয়ার করুন</p>
-                            <ul ng-social-buttons>
-                                 
-                                <li class="ng-social-facebook">Facebook</li>
-                                <li class="ng-social-twitter">Twitter</li>
-                                <li class="ng-social-pinterest">Pinterest</li>
-                            </ul> 
-
-
-                        </div>
-                        
-                    </div>
-
-                    
-
-                    
-
-                        
+                <div class="col-12 col-md-3 text-center">
+                    <h3>Other quizes</h3>
+                </div>
             </div>
+            
+        </div>
+
+                
        
     </div>
 
